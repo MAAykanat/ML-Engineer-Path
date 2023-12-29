f81fc6c8a01cf0c89f3846548589df6272cbe8f0
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2023-12-29 14:44:59 +0300

    pandas useful tools to missing value added

--- Feature Engineering/missing_handle.py
+++ Feature Engineering/missing_handle.py
@@ -2,4 +2,15 @@ import dataset_handle as dh
 
 df_titanic = dh.load_dataset("titanic.csv")
 
-dh.dataset_details(df_titanic)
+df_titanic.loc[[3]] # Specific row(3th) of the dataset 
+
+df_titanic.isnull() # True-False matrix for null values
+df_titanic.notnull() # True-False matrix for non-null values
+
+df_titanic.isnull().sum() # number of null values in each column
+df_titanic.notnull().sum() # number of non-null values in each column
+
+df_titanic.isnull().sum().sort_values(ascending=False) # Sort the number of null values in each column
+((df_titanic.isnull().sum() / df_titanic.shape[0])*100).sort_values(ascending=False) # Sort the percentage of null values in each column
+
+na_columns = [col for col in df_titanic.columns if df_titanic[col].isnull().sum() > 0]
