f33e57647e6a6423dcf63987f4818a5ba6a3133d
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2023-12-29 15:10:03 +0300

    Missing value table funtion added

--- Feature Engineering/missing_handle.py
+++ Feature Engineering/missing_handle.py
@@ -1,10 +1,19 @@
 import dataset_handle as dh
 
-def missing_values_table(dataframe):
-    
-    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+import pandas as pd
+import numpy as np
 
-    return na_columns    
+def missing_values_table(dataframe, null_columns_name = False):
+    null_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+
+    number_of_missing_values = dataframe[null_columns].isnull().sum().sort_values(ascending=False)
+    percentage_of_missing_values = (dataframe[null_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+
+    missing_values_table = pd.concat([number_of_missing_values, np.round(percentage_of_missing_values, 2)], axis=1, keys=["n_miss", "ratio"])
+    print(missing_values_table)
+    
+    if null_columns_name:
+        return null_columns    
 
 df_titanic = dh.load_dataset("titanic.csv")
 
@@ -19,6 +28,10 @@ df_titanic.notnull().sum() # number of non-null values in each column
 df_titanic.isnull().sum().sort_values(ascending=False) # Sort the number of null values in each column
 ((df_titanic.isnull().sum() / df_titanic.shape[0])*100).sort_values(ascending=False) # Sort the percentage of null values in each column
 
-# na_columns = [col for col in df_titanic.columns if df_titanic[col].isnull().sum() > 0]
+na_columns = [col for col in df_titanic.columns if df_titanic[col].isnull().sum() > 0]
 
 print(missing_values_table(df_titanic))
+
+# print(n_miss)
+# print("*"*50)
+# print(ratio)
