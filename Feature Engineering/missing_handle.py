a28d2cab466b412f9d84cfa34764346840acc951
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2023-12-29 14:56:45 +0300

    missing value table function added

--- Feature Engineering/missing_handle.py
+++ Feature Engineering/missing_handle.py
@@ -1,5 +1,14 @@
 import dataset_handle as dh
 
+def missing_values_table(dataframe):
+    
+    na_columns = []
+
+    for col in dataframe.columns:
+        if dataframe[col].isnull().sum() > 0:
+            na_columns.append(col)
+    return na_columns    
+
 df_titanic = dh.load_dataset("titanic.csv")
 
 df_titanic.loc[[3]] # Specific row(3th) of the dataset 
@@ -13,4 +22,6 @@ df_titanic.notnull().sum() # number of non-null values in each column
 df_titanic.isnull().sum().sort_values(ascending=False) # Sort the number of null values in each column
 ((df_titanic.isnull().sum() / df_titanic.shape[0])*100).sort_values(ascending=False) # Sort the percentage of null values in each column
 
-na_columns = [col for col in df_titanic.columns if df_titanic[col].isnull().sum() > 0]
+# na_columns = [col for col in df_titanic.columns if df_titanic[col].isnull().sum() > 0]
+
+print(missing_values_table(df_titanic))
