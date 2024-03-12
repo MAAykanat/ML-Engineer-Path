b1c685ab3d603b4d115b80d688eeb5d48796ddb4
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-12 10:30:47 +0300

    First Glimpse

--- Python for Data Science/pandas_list_comprehension.py
+++ Python for Data Science/pandas_list_comprehension.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+df_car_crush = sns.load_dataset("car_crashes")
+
+print(df_car_crush.head())
+print(df_car_crush.info())
+print(df_car_crush.describe().T)
