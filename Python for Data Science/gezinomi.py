e754e2ab592e47fbbbd895b773339d2e2d930611
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-13 13:52:11 +0300

    Read dataset

--- Python for Data Science/gezinomi.py
+++ Python for Data Science/gezinomi.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import seaborn as sns
 
-PATH = "D:\!!!MAAykanat Dosyalar\Miuul\Python for Data Science\Görevler\A3-Gezinomi Kural Tabanlı Sınıflandırma ile Potansiyel Müşteri Getirisi Hesaplama\gezinomi_tanıtım"
-
-df = pd.read_excel(PATH + "\gezinomi_tanıtım.xlsx")
+df = pd.read_excel(".\Python for Data Science\datasets\miuul_gezinomi.xlsx")
 print(df.head())
