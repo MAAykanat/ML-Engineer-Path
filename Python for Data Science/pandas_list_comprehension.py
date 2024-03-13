0c6c197061e3847ade1ad2a96e4ea271e6abc40f
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-13 10:12:20 +0300

    Task-22 is done

--- Python for Data Science/pandas_list_comprehension.py
+++ Python for Data Science/pandas_list_comprehension.py
@@ -155,8 +155,12 @@ print(df_tips.groupby(["day","time"]).agg({"total_bill": ["sum", "min", "max", "
 
 #Task 20:
 print(df_tips[(df_tips["time"]=="Lunch") & (df_tips["sex"]== "Female")].head(50).sort_values(by=["day"], ascending=False))
-print(df_tips[(df_tips["time"]=="Lunch") & (df_tips["sex"]== "Female")].groupby(["day"]).agg({"total_bill": ["sum", "min", "max", "mean"]
+print(df_tips[(df_tips["time"]=="Lunch") & (df_tips["sex"]== "Female")].groupby(["day"]).agg({"total_bill": ["sum", "min", "max", "mean"],
                                                                                               "tip": ["sum", "min", "max", "mean"]}))
 
 #Task 21:
 print(df_tips.loc[(df_tips["size"]<3) & (df_tips["total_bill"]>10)].mean())
+
+#Task 22:
+df_tips["total_bill_tip_sum"] = df_tips["total_bill"] + df_tips["tip"]
+print(df_tips.head())
