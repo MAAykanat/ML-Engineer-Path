2315c93c6454bce3113939b9e94030a42d26ff74
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-13 10:20:12 +0300

    check_df changed to more efficient

--- Feature Engineering/dataset_handle.py
+++ Feature Engineering/dataset_handle.py
@@ -14,13 +14,15 @@ def load_dataset(dataset_name, path=PATH):
 def check_df(dataframe, head=5):
     print("Dataframe shape: ", dataframe.shape)
     print("*"*50)
-    print("Dataframe info: \n", dataframe.info())
-    print("*"*50)
-    print("Dataframe describe: \n", dataframe.describe().T)
+    print("Dataframe coloumn types: ", dataframe.dtypes)
     print("*"*50)
     print("Dataframe head: \n", dataframe.head(head))
     print("*"*50)
     print("Dataframe tail: \n", dataframe.tail(head))
+    print("*"*50)
+    print("Dataframe info: \n", dataframe.info())
+    print("*"*50)
+    print("Dataframe describe: \n", dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
     print("*"*50)
     print("Dataframe missing values: \n", dataframe.isnull().sum())
 
