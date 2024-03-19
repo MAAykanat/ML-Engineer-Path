5f3f6ff3640949ecfc2890730ece5576aff6f60f
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-19 13:34:15 +0300

    General picture of dataset

--- Machine Learning/linear_regression.py
+++ Machine Learning/linear_regression.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import numpy as np
 
-# Importing the dataset
-df = pd.read_csv('datasets/advertising.csv')
+def check_df(dataframe, head=5):
+    print("Dataframe shape: ", dataframe.shape)
+    print("*"*50)
+    print("Dataframe coloumn types: ", dataframe.dtypes)
+    print("*"*50)
+    print("Dataframe head: \n", dataframe.head(head))
+    print("*"*50)
+    print("Dataframe tail: \n", dataframe.tail(head))
+    print("*"*50)
+    print("Dataframe info: \n", dataframe.info())
+    print("*"*50)
+    print("Dataframe describe: \n", dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+    print("*"*50)
+    print("Dataframe missing values: \n", dataframe.isnull().sum())
 
+# Importing the dataset
+df = pd.read_csv('Machine Learning/datasets/advertising.csv')
+
+check_df(df)
