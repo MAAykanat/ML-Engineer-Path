14687d01e76053a1cab1dd5e3a0518b3b2e5dc31
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-20 15:11:12 +0300

    EDA-Step1 General Picture of dataset

--- Machine Learning/Hands_on/house_price_prediction.py
+++ Machine Learning/Hands_on/house_price_prediction.py
@@ -3,5 +3,41 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+pd.set_option('display.max_columns', None)
+pd.set_option('max_colwidth', None)
+pd.set_option('display.max_rows', 20)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
 df = pd.read_csv("Machine Learning/datasets/house_pricing/train.csv")
 print(df.head())
+
+#######################################
+### EXPLORATORY DATA ANALYSIS - EDA ###
+#######################################
+
+# 1. General Picture of the Dataset
+# 2. Catch Numeric and Categorical Value
+# 3. Catetorical Variables Analysis
+# 4. Numeric Variables Analysis
+# 5. Target Variable Analysis (Dependent Variable) - Categorical
+# 6. Target Variable Analysis (Dependent Variable) - Numeric
+# 7. Outlier Detection
+# 8. Missing Value Analysis
+# 9. Correlation Matrix
+
+# 1. General Picture of the Dataset
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
