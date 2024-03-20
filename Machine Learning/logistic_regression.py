782d1f3ae0f6874ee0a5adc99d981d810576798c
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-20 11:19:01 +0300

    EDA-Step1 and Step2 is done.

--- Machine Learning/logistic_regression.py
+++ Machine Learning/logistic_regression.py
@@ -13,3 +13,84 @@ pd.set_option('display.float_format', lambda x: '%.3f' % x)
 df = pd.read_csv('Machine Learning/datasets/diabetes.csv')
 
 print(df.head())
+
+#######################################
+### EXPLORATORY DATA ANALYSIS - EDA ###
+#######################################
+
+# 1. General Picture of the Dataset
+# 2. Catch Numeric and Categorical Value
+# 3. Catetorical Variables Analysis
+# 4. Numeric Variables Analysis
+# 5. Target Variable Analysis (Dependent Variable) - Categorical
+# 6. Target Variable Analysis (Dependent Variable) - Numeric
+# 7. Outlier Detection
+# 8. Missing Value Analysis
+# 9. Correlation Matrix
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+
+# 2. Catch Numeric and Categorical Value
+
+def grap_column_names(dataframe, categorical_th=10, cardinal_th=20):
+    """
+    It gives the names of categorical, numerical and categorical but cardinal variables in the data set.
+    Note: Categorical variables with numerical appearance are also included."""
+
+    """
+    Cardinal Variables: Variables that are categorical and do not carry information,
+    that is, have too many classes, are called variables with high cardinality.
+    """
+
+    """
+    Returns
+    ------
+        categorical_cols: list
+                Categorical variable list
+        num_cols: list
+                Numeric variable list
+        categorical_but_cardinal: list
+                Categorical variables with high cardinality list
+    """
+    # categorical_cols, categorical_but_cardinal
+    categorical_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    numeric_but_categorical = [col for col in dataframe.columns if dataframe[col].nunique() < categorical_th and
+                   dataframe[col].dtypes != "O"]
+    categorical_but_cardinal = [col for col in dataframe.columns if dataframe[col].nunique() > cardinal_th and
+                   dataframe[col].dtypes == "O"]
+    categorical_cols = categorical_cols + numeric_but_categorical
+    categorical_cols = [col for col in categorical_cols if col not in categorical_but_cardinal]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in numeric_but_categorical]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'categorical_cols: {len(categorical_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'categorical_but_cardinal: {len(categorical_but_cardinal)}')
+    print(f'numeric_but_categorical: {len(numeric_but_categorical)}')
+
+    return categorical_cols, num_cols, categorical_but_cardinal
+
+cat_cols, num_cols, cat_but_car = grap_column_names(df)
+
+print("Categorical Columns: \n\n", cat_cols)
+print("Numeric Columns: \n\n", num_cols)
+[print("Categorical but Cardinal EMPTY!!!\n\n") if cat_but_car == [] else print("Categorical but Cardinal: \n", cat_but_car)]
+print("#"*50)
