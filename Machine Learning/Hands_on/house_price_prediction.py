14eb2f5c0e24ed7ac6b3e6a6a4f30b025fc49520
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-20 14:54:49 +0300

    HousePricing dataset import

---
+++ Machine Learning/Hands_on/house_price_prediction.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("Machine Learning/datasets/house_pricing/train.csv")
+print(df.head())
