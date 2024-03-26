c74e18584681e6896931767a3d50494d76647363
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-26 14:09:02 +0300

    Cleaned dataset import

--- Machine Learning/knn.py
+++ Machine Learning/knn.py
@@ -3,3 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Cleaned dataeset import
+df = pd.read_csv("Machine Learning/datasets/diabetes_cleaned.csv")
+print(df.head())
+df = df.reindex(sorted(df.columns), axis=1)
