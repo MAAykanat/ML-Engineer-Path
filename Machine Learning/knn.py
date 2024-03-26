4ff9f61ce7ea085f18765415f64ecb289e7366bd
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-26 14:26:57 +0300

    Hyperparameter optimization

--- Machine Learning/knn.py
+++ Machine Learning/knn.py
@@ -4,9 +4,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.model_selection import cross_validate
+
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -41,3 +43,18 @@ cv = cross_validate(estimator=knn_model, X=X, y=y, cv=10, scoring=["accuracy", "
 print("Accuracy: ", cv["test_accuracy"].mean())
 print("F1: ", cv["test_f1"].mean())
 print("ROC AUC: ", cv["test_roc_auc"].mean())
+
+#################################
+## Hyperparameter Optimization ##
+#################################
+
+print(knn_model.get_params())
+knn_params = {"n_neighbors": np.arange(2, 100), 
+              "p": [1, 2, 3]}
+knn_gridSearch_model = GridSearchCV(estimator=knn_model, param_grid=knn_params, cv=10, n_jobs=-1, verbose=1).fit(X, y)
+
+print(knn_gridSearch_model.best_params_)
+
+"""
+{'n_neighbors': 73, 'p': 1}
+"""
