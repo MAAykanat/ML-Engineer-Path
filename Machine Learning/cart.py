689a4b1fcca1d342f8c8c8c4e913ac0c5d91e0d1
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-27 12:21:41 +0300

    Arrange pandas setups

--- Machine Learning/cart.py
+++ Machine Learning/cart.py
@@ -4,6 +4,11 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 
+pd.set_option('display.max_columns', None)
+pd.set_option('max_colwidth', None)
+pd.set_option('display.max_rows', 20)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
 df = pd.read_csv("Machine Learning/datasets/diabetes_cleaned.csv")
 print(df.head())
 
