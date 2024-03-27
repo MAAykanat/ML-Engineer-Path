a5bdaa4c4c79ac840b3ce2944d5c444f632f7857
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-27 13:51:49 +0300

    Hold out implemented

--- Machine Learning/cart.py
+++ Machine Learning/cart.py
@@ -3,6 +3,8 @@ import pandas as pd
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, roc_auc_score, roc_curve, auc
 
 pd.set_option('display.max_columns', None)
 pd.set_option('max_colwidth', None)
@@ -16,9 +18,25 @@ print(df.head())
 ### MODELLING ###
 #################
 
+####################
+## Holdout Method ##
+####################
+
 # 1. Splitting the data
 y = df['Outcome']
 X = df.drop('Outcome', axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+cart_model = DecisionTreeClassifier(random_state=42).fit(X_train, y_train)
+
+# 2. Prediction
+# for Confusion Matrix y_pred
+y_pred = cart_model.predict(X_test)
+# for AUC y_prob
+y_prob = cart_model.predict_proba(X_test)[:, 1]
+
+print(classification_report(y_test,y_pred))
+print("Accuracy: ", accuracy_score(y_test, y_pred))
+print("AUC: ", roc_auc_score(y_test, y_prob))
 
