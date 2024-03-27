817536c38c561553d89f56d4349bae1fc6c4b7ba
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-03-27 12:20:27 +0300

    Split dataset

--- Machine Learning/cart.py
+++ Machine Learning/cart.py
@@ -2,5 +2,18 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from sklearn.model_selection import train_test_split
+
 df = pd.read_csv("Machine Learning/datasets/diabetes_cleaned.csv")
 print(df.head())
+
+#################
+### MODELLING ###
+#################
+
+# 1. Splitting the data
+y = df['Outcome']
+X = df.drop('Outcome', axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
