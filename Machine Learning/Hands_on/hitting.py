20e6aa4442e78141e772cc70ddd633f5e933a6df
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-04-02 09:49:20 +0300

    EDA-General Picture of Dataset

--- Machine Learning/Hands_on/hitting.py
+++ Machine Learning/Hands_on/hitting.py
@@ -5,3 +5,42 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("Machine Learning/datasets/hitter/hitters.csv")
 print(df.head())
+
+#######################################
+### EXPLORATORY DATA ANALYSIS - EDA ###
+#######################################
+
+# 1. General Picture of the Dataset
+# 2. Catch Numeric and Categorical Value
+# 3. Catetorical Variables Analysis
+# 4. Numeric Variables Analysis
+# 5. Target Variable Analysis (Dependent Variable) - Categorical
+# 6. Target Variable Analysis (Dependent Variable) - Numeric
+# 7. Outlier Detection
+# 8. Missing Value Analysis
+# 9. Correlation Matrix
+
+# 1. General Picture of the Dataset
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+# Shape: (322,20)
+# Target: Salary (Numeric) - float64
+# League, Division, NewLeague (Categorical) - object
+# Others: 16 Numeric - int64 
+
+# Only salary has missing values (59)
+# There can be outliers!!!
