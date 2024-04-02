4743b04ab2beafabe374e43da8dac586c2b7e092
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-04-02 09:51:53 +0300

    EDA-Catch cat_cols and num_cols

--- Machine Learning/Hands_on/hitting.py
+++ Machine Learning/Hands_on/hitting.py
@@ -44,3 +44,62 @@ check_df(df)
 
 # Only salary has missing values (59)
 # There can be outliers!!!
+
+# 2. Catch Numeric and Categorical Value
+"""
+Observations: 322
+Variables: 20
+categorical_cols: 3
+num_cols: 17
+categorical_but_cardinal: 0
+numeric_but_categorical: 0
+"""
+
+def grap_column_names(dataframe, categorical_th=10, cardinal_th=20):
+    """
+    It gives the names of categorical, numerical and categorical but cardinal variables in the data set.
+    Note: Categorical variables with numerical appearance are also included."""
+
+    """
+    Cardinal Variables: Variables that are categorical and do not carry information,
+    that is, have too many classes, are called variables with high cardinality.
+    """
+
+    """
+    Returns
+    ------
+        categorical_cols: list
+                Categorical variable list
+        num_cols: list
+                Numeric variable list
+        categorical_but_cardinal: list
+                Categorical variables with high cardinality list
+    """
+    # categorical_cols, categorical_but_cardinal
+    categorical_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    numeric_but_categorical = [col for col in dataframe.columns if dataframe[col].nunique() < categorical_th and
+                   dataframe[col].dtypes != "O"]
+    categorical_but_cardinal = [col for col in dataframe.columns if dataframe[col].nunique() > cardinal_th and
+                   dataframe[col].dtypes == "O"]
+    categorical_cols = categorical_cols + numeric_but_categorical
+    categorical_cols = [col for col in categorical_cols if col not in categorical_but_cardinal]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in numeric_but_categorical]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'categorical_cols: {len(categorical_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'categorical_but_cardinal: {len(categorical_but_cardinal)}')
+    print(f'numeric_but_categorical: {len(numeric_but_categorical)}')
+
+    return categorical_cols, num_cols, categorical_but_cardinal
+
+cat_cols, num_cols, cat_but_car = grap_column_names(df)
+
+print("Categorical Columns: \n\n", cat_cols)
+print("Numeric Columns: \n\n", num_cols)
+[print("Categorical but Cardinal EMPTY!!!\n\n") if cat_but_car == [] else print("Categorical but Cardinal: \n", cat_but_car)]
+print("#"*50)
