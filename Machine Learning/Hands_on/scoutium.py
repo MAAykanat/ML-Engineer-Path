2db70355a2c21ddcd757b040f9331887b824fef2
Muhammet Ali Aykanat <m.a.aykanat@gmail.com>
2024-04-05 10:53:07 +0300

    General picture of dataset, only one object others are numeric and no na value

--- Machine Learning/Hands_on/scoutium.py
+++ Machine Learning/Hands_on/scoutium.py
@@ -31,5 +31,34 @@ print(table.head())
 table.reset_index(inplace=True)
 print(table.head())
 
+#######################################
+### EXPLORATORY DATA ANALYSIS - EDA ###
+#######################################
 
-# , fill_value=0)
+# 1. General Picture of the Dataset
+# 2. Catch Numeric and Categorical Value
+# 3. Catetorical Variables Analysis
+# 4. Numeric Variables Analysis
+# 5. Target Variable Analysis (Dependent Variable) - Categorical
+# 6. Target Variable Analysis (Dependent Variable) - Numeric
+# 7. Outlier Detection
+# 8. Missing Value Analysis
+# 9. Correlation Matrix
+
+# 1. General Picture of the Dataset
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(table)
