556d5a2cb2c33610477dddf36800c53919ef6863
MAAykanat <m.a.aykanat@gmail.com>
2024-09-18 09:53:44 +0300

    Spark regression implementation

--- Spark Machine Learning/regression.py
+++ Spark Machine Learning/regression.py
@@ -1,2 +1,13 @@
 import numpy as np
 import pandas as pd
+
+from pyspark.sql import SparkSession
+import findspark
+
+findspark.init()
+
+spark = SparkSession.builder.appName("regression").getOrCreate()
+
+df = spark.read.csv("Machine Learning/datasets/telco/Telco-Customer-Churn.csv", header=True, inferSchema=True)
+
+print(df.printSchema())
